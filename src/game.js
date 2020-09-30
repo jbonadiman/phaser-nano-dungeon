99e4e08cdfc7eb7b8bc90f9b6033996c8deb5fdb
João Vitor Bonadiman <j.bonadiman@live.com>
2020-09-30 00:46:45 -0300

    -Finished second chapter and exercise;

--- src/game.js
+++ src/game.js
@@ -7,9 +7,34 @@ const scene = {
   },
 
   create: function () {
-    this.add
+    this.helloText = this.add
       .bitmapText(400, 300, "arcade", "Hello Phaser")
       .setOrigin(0.5);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+  },
+
+  update: function() {
+    if (this.helloText.x > 1000) this.helloText.x = -200;
+    if (this.helloText.x < -200) this.helloText.x = 1000;
+    if (this.helloText.y > 600) this.helloText.y = -50;
+    if (this.helloText.y < -50) this.helloText.y = 600;
+
+    if (this.cursors.left.isDown) {
+      this.helloText.x -= 10;
+    }
+
+    if (this.cursors.right.isDown) {
+      this.helloText.x += 10;
+    }
+
+    if (this.cursors.up.isDown) {
+      this.helloText.y -= 10;
+    }
+
+    if (this.cursors.down.isDown) {
+      this.helloText.y += 10;
+    }
   }
 }
 
